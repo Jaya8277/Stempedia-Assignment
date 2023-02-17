e542af78376c3ff9a0808409db3db6433230373b
Jaya Vishwakarma <jayavish423@gmail.com>
2023-02-17 19:34:14 +0530

    done

--- task1/src/index.js
+++ task1/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import storeReducer from "./Redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Store = createStore(storeReducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
